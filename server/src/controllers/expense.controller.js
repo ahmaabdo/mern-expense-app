1984aafb5d7a381555ee9d261051797cf019f6ed
Ahmad <ahmed.abdel-gaied@appspro-me.com>
2019-02-04 22:08:13 +0200

    add crud operaions

---
+++ server/src/controllers/expense.controller.js
@@ -0,0 +1,98 @@
+const Expense = require('../models/expense.model');
+
+const expenseController = {};
+
+expenseController.get = async (req, res, next) => {
+  const { user } = req;
+
+  const now = new Date();
+  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+  const query = {
+    owner: user._id,
+    created: {
+      $gte: firstDay,
+      $lt: lastDay
+    }
+  };
+
+  try {
+    const expense = await Expense.find(query);
+    return res.send({
+      expense
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+expenseController.create = async (req, res, next) => {
+  const { amount, description, created } = req.body;
+  const newExpense = new Expense({
+    amount,
+    description,
+    created,
+    owner: req.user
+  });
+
+  try {
+    const saved = await newExpense.save();
+    return res.send({
+      success: true,
+      expense: saved
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+expenseController.update = async (req, res, next) => {
+  const expense_id = req.params.expense_id;
+  const { amount, description, created } = req.body;
+
+  try {
+    
+    const check = await Expense.findOne({ _id: expense_id });
+    if (!check.owner.equals(req.user._id)) {
+      const err = new Error('This exepense object does not belong to you!');
+      err.status = 401;
+      console.log('error');
+      throw err;
+    }
+
+    const expense = await Expense.update(
+      { _id: expense_id },
+      { amount, description, created }
+    );
+    return res.send({
+      success: true,
+      expense
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+expenseController.destroy = async (req, res, next) => {
+  const expense_id = req.params.expense_id;
+  
+  const check = await Expense.findOne({ _id: expense_id });
+  if (!check.owner.equals(req.user._id)) {
+    const err = new Error('This exepense object does not belong to you!');
+    err.status = 401;
+    console.log('error');
+    throw err;
+  }
+
+  try {
+    await Expense.deleteOne({ _id: expense_id });
+    res.send({
+      success: true
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = expenseController;
